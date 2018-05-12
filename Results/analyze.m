255393b3734289e1d55106d13060165c2c3be30a
Ivar Eriksen <ivareri@stud.ntnu.no>
2018-05-12 19:40:03 +0200

    Unpushed changes

--- Results/analyze.m
+++ Results/analyze.m
@@ -1,4 +1,4 @@
-function [test,maximum,minimum,med,expected,variance,deviation] = analyze(filename)
+function [test,maximum,minimum,med,expected,variance,deviation, result] = analyze(filename)
 
 %filename = 'C++ - single thread - E6AXIS';
 %filename = 'C++ - single thread - INT';
@@ -10,9 +10,9 @@ function [test,maximum,minimum,med,expected,variance,deviation] = analyze(filena
 %filename = 'Java - 5 thread - INT';
 
 % Outlier limit
-lim_read = 100;
-lim_write = 100;
-lim_sync = 100;
+lim_read = 30;
+lim_write = 30;
+lim_sync = 30;
 
 %% Code
 
@@ -22,20 +22,24 @@ hold all;
 
 
 %% Read
-%figure('visible','off');
-%clf;
+figure(1);
+clf;
 rows = strcmp(data.Testtype, 'Read') & (data.Times*1000 > lim_read);
+read_rows = sum(strcmp(data.Testtype, 'Read'));
+
 read_outliers = (data(rows,'Times').Times*1000)';
+read_outliers_precent = sum(rows) / read_rows * 100;
+
 
 rows = strcmp(data.Testtype, 'Read') & (data.Times*1000 < lim_read);
-%histogram(data(rows,'Times').Times*1000);
+histogram(data(rows,'Times').Times*1000);
 maximum(1,1)  = max(data(rows,'Times').Times*1000);
 minimum(1,1)   = min(data(rows,'Times').Times*1000);
 med(1,1) = median(data(rows,'Times').Times*1000);
 expected(1,1) = mean(data(rows,'Times').Times*1000);
 variance(1,1) = var(data(rows,'Times').Times*1000);
 deviation(1,1) = std(data(rows,'Times').Times*1000);
-%axis tight;
+axis tight;
 
 %print(f, '-deps', strcat(filename, ' - plot read.eps'));
 
@@ -43,45 +47,86 @@ deviation(1,1) = std(data(rows,'Times').Times*1000);
 %figure(2);
 %clf;
 rows = strcmp(data.Testtype, 'Write') & (data.Times*1000 > lim_write);
-write_outliers = (data(rows,'Times').Times*1000)';
+write_rows = sum(strcmp(data.Testtype, 'Write'));
+if any(rows)
+    write_outliers = (data(rows,'Times').Times*1000)';
+    write_outliers_precent = sum(rows) / write_rows * 100;
+else
+    write_outliers = 0;
+    write_outliers_precent = 0;
+end
 
 rows = strcmp(data.Testtype, 'Write') & (data.Times*1000 < lim_write);
-%histogram(data(rows,'Times').Times*1000);
+if any(rows)
+histogram(data(rows,'Times').Times*1000);
 maximum(2,1)  = max(data(rows,'Times').Times*1000);
 minimum(2,1)   = min(data(rows,'Times').Times*1000);
 expected(2,1) = mean(data(rows,'Times').Times*1000);
 med(2,1) = median(data(rows,'Times').Times*1000);
 variance(2,1) = var(data(rows,'Times').Times*1000);
 deviation(2,1) = std(data(rows,'Times').Times*1000);
-%axis tight;
-%print('-deps', strcat(filename, ' - plot write.eps'));
-
+axis tight;
+print('-deps', strcat(filename, ' - plot write.eps'));
+else
+    maximum(3,1) = 0;
+    minimum(3,1) = 0;
+    expected(3,1) = 0;
+    med(3,1) = 0;
+    variance(3,1) = 0;
+    deviation(3,1) = 0;
+end
 
 %% sync_write read
 %figure(3);
 %clf;
 rows = strcmp(data.Testtype, 'sync_write_read') & (data.Times*1000 > lim_sync);
-sync_outliers = (data(rows,'Times').Times*1000)';
+sync_rows = sum(strcmp(data.Testtype, 'sync_write_read'));
+
+if any(rows)
+    sync_outliers = (data(rows,'Times').Times*1000)';
+    sync_outliers_precent = sum(rows) / sync_rows * 100;
+else
+    sync_outliers = 0;
+    sync_outliers_precent = 0;
+end
 
 rows = strcmp(data.Testtype, 'sync_write_read') & (data.Times*1000 < lim_sync);
-%histogram(data(rows,'Times').Times*1000);
-maximum(3,1)  = max(data(rows,'Times').Times*1000);
-minimum(3,1)   = min(data(rows,'Times').Times*1000);
-expected(3,1) = mean(data(rows,'Times').Times*1000);
-med(3,1) = median(data(rows,'Times').Times*1000);
-variance(3,1) = var(data(rows,'Times').Times*1000);
-deviation(3,1) = std(data(rows,'Times').Times*1000);
-%axis tight;
-%print('-deps', strcat(filename, ' - plot sync.eps'));
+if any(rows)
+    %histogram(data(rows,'Times').Times*1000);
+    maximum(3,1)  = max(data(rows,'Times').Times*1000);
+    minimum(3,1)   = min(data(rows,'Times').Times*1000);
+    expected(3,1) = mean(data(rows,'Times').Times*1000);
+    med(3,1) = median(data(rows,'Times').Times*1000);
+    variance(3,1) = var(data(rows,'Times').Times*1000);
+    deviation(3,1) = std(data(rows,'Times').Times*1000);
+    %axis tight;
+    %print('-deps', strcat(filename, ' - plot sync.eps'));
+else
+    maximum(3,1) = 0;
+    minimum(3,1) = 0;
+    expected(3,1) = 0;
+    med(3,1) = 0;
+    variance(3,1) = 0;
+    deviation(3,1) = 0;
+end
 
 %% Output
 test = {'Read';'Write';'Sync'};
 result = table(test,maximum,minimum,med,expected,variance,deviation);
 writetable(result, strcat('Matlab output/',filename, ' - Table.csv'));
 
-outliers = sprintf('Read outliers [%d]: %s \nWrite outliers [%d]: %s \nSync outliers [%d]: %s \n', ...
+outliers = sprintf('%i of %i (%i %%) Read outliers [%dms]: %s \n%i of %i (%i %%) Write outliers [%dms]: %s \n%i of %i(%i %%) Sync outliers [%dms]: %s \n', ...
+                numel(read_outliers), ...
+                read_rows, ...
+                read_outliers_precent, ...
                 lim_read, mat2str(read_outliers), ...
+                numel(write_outliers), ...
+                write_rows, ...
+                write_outliers_precent, ...
                 lim_write, mat2str(write_outliers), ...
+                numel(sync_outliers), ...
+                sync_rows, ...
+                sync_outliers_precent, ...
                 lim_sync, mat2str(sync_outliers));
             
 fid = fopen(strcat('Matlab output/',filename, ' - Outliers.txt'),'w');
@@ -92,3 +137,4 @@ fclose(fid);
 %% Print stuff to terminal
 result
 outliers
+
